04fec85788adca5803b358e357209d48bf6cb214
Igor Ryabkov <Igor_Ryabkov_1@mail.ru>
2024-10-12 04:52:09 +0300

    Add bpe

--- src/text_encoder/ctc_text_encoder.py
+++ src/text_encoder/ctc_text_encoder.py
@@ -1,4 +1,7 @@
 import re
+import os, shutil
+import json
+from tokenizers import ByteLevelBPETokenizer
 from string import ascii_lowercase
 
 import torch
@@ -13,7 +16,7 @@ import torch
 class CTCTextEncoder:
     EMPTY_TOK = ""
 
-    def __init__(self, alphabet=None, **kwargs):
+    def __init__(self, alphabet=None, vocab_size=None, force=True, tokenizer_type='character_wise', **kwargs):
         """
         Args:
             alphabet (list): alphabet for language. If None, it will be
@@ -24,22 +27,62 @@ class CTCTextEncoder:
             alphabet = list(ascii_lowercase + " ")
 
         self.alphabet = alphabet
-        self.vocab = [self.EMPTY_TOK] + list(self.alphabet)
+        self.tokenizer_type = tokenizer_type
 
-        self.ind2char = dict(enumerate(self.vocab))
-        self.char2ind = {v: k for k, v in self.ind2char.items()}
+        if tokenizer_type == 'character_wise':
+            self.vocab = [self.EMPTY_TOK] + list(self.alphabet)
+            self.ind2token = dict(enumerate(self.vocab))
+            self.token2ind = {v: k for k, v in self.ind2char.items()}
 
+        # REDO:
+        elif tokenizer_type == 'bpe':
+            PATH = './data/datasets/librispeech/train-clean-100_index.json'
+            TEMP_CORPUS_PATH = './src/text_encoder/bpe/temp_corpus.txt'
+            MODEL_PATH = './src/text_encoder/bpe/'
+            special_tokens = ["<unk>", "EMPTY_TOKEN"]
+
+            if force:
+                shutil.rmtree(MODEL_PATH)
+                os.mkdir(MODEL_PATH)
+
+            with open(PATH) as f:
+                data = json.load(f)
+                corpus = '\n'.join([el['text'] for el in data])
+                
+            with open(TEMP_CORPUS_PATH, 'w+') as f:
+                f.write(corpus)
+
+            tokenizer = ByteLevelBPETokenizer()
+            tokenizer.train(files=TEMP_CORPUS_PATH, vocab_size=vocab_size, show_progress=True, special_tokens=special_tokens)
+            tokenizer.save_model(MODEL_PATH, 'bpe_tokenizer_new')
+
+            os.remove(TEMP_CORPUS_PATH)
+
+            self.tokenizer = ByteLevelBPETokenizer(
+                f"{MODEL_PATH}bpe_tokenizer_new-vocab.json",
+                f"{MODEL_PATH}bpe_tokenizer_new-merges.txt",
+            ) 
+
+            self.vocab = self.tokenizer.get_vocab()
+            
     def __len__(self):
         return len(self.vocab)
 
     def __getitem__(self, item: int):
         assert type(item) is int
-        return self.ind2char[item]
+        if self.tokenizer_type == 'character_wise':
+            return self.ind2char[item]
 
     def encode(self, text) -> torch.Tensor:
         text = self.normalize_text(text)
         try:
-            return torch.Tensor([self.char2ind[char] for char in text]).unsqueeze(0)
+            if self.tokenizer_type == 'character_wise':
+                return torch.Tensor([self.char2ind[char] for char in text]).unsqueeze(0)
+            else:
+                # print(text)
+                tokenized = self.tokenizer.encode(text).ids
+                # print(torch.tensor(tokenized))
+                return torch.tensor(tokenized)
         except KeyError:
             unknown_chars = set([char for char in text if char not in self.char2ind])
             raise Exception(
@@ -56,7 +99,10 @@ class CTCTextEncoder:
         Returns:
             raw_text (str): raw text with empty tokens and repetitions.
         """
-        return "".join([self.ind2char[int(ind)] for ind in inds]).strip()
+        if self.tokenizer_type == 'character_wise':
+            return "".join([self.ind2char[int(ind)] for ind in inds]).strip()
+        else:
+            return self.tokenizer.decode(inds)
 
     def ctc_decode(self, inds) -> str:
         # ind -> chars
@@ -64,7 +110,9 @@ class CTCTextEncoder:
 
         last_ind = None
         for i in range(len(inds)):
-            if i == 0 or (inds[i] != inds[i - 1] and inds[i] != self.char2ind[self.EMPTY_TOK]):
+            if  i == 0 or inds[i] != inds[i - 1] and \
+                (self.tokenizer_type == 'character_wise' and inds[i] != self.char2ind[self.EMPTY_TOK] or \
+                self.tokenizer_type == 'bpe' and inds[i] != self.tokenizer.token_to_id(self.EMPTY_TOK)):
                 res_list.append(inds[i])
 
         return self.decode(res_list)
